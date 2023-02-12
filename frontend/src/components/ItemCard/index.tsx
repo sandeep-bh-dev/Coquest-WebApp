ccd06f500026e532b0f4898c24969ae146404ee0
Jesse Atkinson <124186705+JesseA7@users.noreply.github.com>
2023-02-12 23:27:03 +0000

    fixed indent tab size

--- frontend/src/components/ItemCard/index.tsx
+++ frontend/src/components/ItemCard/index.tsx
@@ -7,49 +7,49 @@ import Typography from '@mui/material/Typography';
 import ItemMenu from '../ItemMenu';
 
 type ItemCardProps = CardProps & {
-  taskName: string;
-  itemName: string;
+    taskName: string;
+    itemName: string;
 };
 
 const Title = styled(Typography)({
-  variant: "overline",
-  fontWeight: 400,
-  fontSize: 12,
-  lineHeight: "18px"
+    variant: "overline",
+    fontWeight: 400,
+    fontSize: 12,
+    lineHeight: "18px"
 });
 
 const Subtitle = styled(Typography)({
-  variant: "subtitle2",
-  fontWeight: 600,
-  fontSize: 16,
-  lineHeight: "24px"
+    variant: "subtitle2",
+    fontWeight: 600,
+    fontSize: 16,
+    lineHeight: "24px"
 });
 
 const ItemCard = ({
-  taskName,
-  itemName,
+    taskName,
+    itemName,
 }: ItemCardProps) => {
-  const CustomCard = styled(Card)({
-    height: 254,
-    width: 248.43,
-  });
-  return (
-    <CustomCard>
-      <CardMedia style={{ backgroundColor: 'lightgrey' }}
-        component="img"
-        height="186"
-        image=""
-        alt=""
-      />
-      <CardHeader
-        action={
-          <ItemMenu />
-        }
-        title={<Title>{taskName}</Title>}
-        subheader={<Subtitle>{itemName}</Subtitle>}
-      />
-    </CustomCard>
-  );
+    const CustomCard = styled(Card)({
+        height: 254,
+        width: 248.43,
+    });
+    return (
+        <CustomCard>
+            <CardMedia style={{ backgroundColor: 'lightgrey' }}
+                component="img"
+                height="186"
+                image=""
+                alt=""
+            />
+            <CardHeader
+                action={
+                    <ItemMenu />
+                }
+                title={<Title>{taskName}</Title>}
+                subheader={<Subtitle>{itemName}</Subtitle>}
+            />
+        </CustomCard>
+    );
 }
 
 export default ItemCard;
