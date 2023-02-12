f1bdb32874211160ebf5d58d29b18443960562eb
Jesse Atkinson <124186705+JesseA7@users.noreply.github.com>
2023-02-12 23:29:07 +0000

    fixed indent tab size

--- frontend/src/components/ItemGrid/index.tsx
+++ frontend/src/components/ItemGrid/index.tsx
@@ -3,58 +3,58 @@ import styled from "@emotion/styled";
 import ItemCard from "../ItemCard";
 
 const Title = styled.h1({
-  width: '100%',
-  textAlign: 'left',
-  fontWeight: 600,
-  fontSize: 36,
-  marginTop: '60px',
-  marginBottom: '20px',
-  marginLeft: '21%',
-  fontFamily: 'Poppins'
+    width: '100%',
+    textAlign: 'left',
+    fontWeight: 600,
+    fontSize: 36,
+    marginTop: '60px',
+    marginBottom: '20px',
+    marginLeft: '21%',
+    fontFamily: 'Poppins'
 });
 
 const Container = styled.div({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  position: 'relative'
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative'
 });
 
 const Grid = styled.div({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)',
-  gridGap: '16px',
-  width: '80%',
-  maxWidth: '1100px',
-  '@media (max-width: 1000px)': {
-    gridTemplateColumns: 'repeat(3, 1fr)'
-  },
-  '@media (max-width: 800px)': {
-    gridTemplateColumns: 'repeat(2, 1fr)'
-  },
-  '@media (max-width: 600px)': {
-    gridTemplateColumns: 'repeat(1, 1fr)'
-  }
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridGap: '16px',
+    width: '80%',
+    maxWidth: '1100px',
+    '@media (max-width: 1000px)': {
+        gridTemplateColumns: 'repeat(3, 1fr)'
+    },
+    '@media (max-width: 800px)': {
+        gridTemplateColumns: 'repeat(2, 1fr)'
+    },
+    '@media (max-width: 600px)': {
+        gridTemplateColumns: 'repeat(1, 1fr)'
+    }
 });
 
 const ItemGrid = () => {
-  return (
-    <>
-      <Title>Inventory</Title>
-      <Container>
-        <Grid>
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-          <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
-        </Grid>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Title>Inventory</Title>
+            <Container>
+                <Grid>
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                    <ItemCard taskName="PROJECT OR TASK NAME" itemName="Item name" />
+                </Grid>
+            </Container>
+        </>
+    );
 };
 
 export default ItemGrid;
--- frontend/src/components/ItemMenu/index.tsx
+++ frontend/src/components/ItemMenu/index.tsx
@@ -6,85 +6,85 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import styled from '@emotion/styled';
 
 const StyledMenuItem = styled(MenuItem)({
-  fontWeight: 400,
-  fontFamily: 'Poppins',
-  fontSize: 16,
-  lineHeight: '24px'
+    fontWeight: 400,
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    lineHeight: '24px'
 });
 
 const StyledMenu = styled(Menu)({
-  '& .MuiPaper-root': {
-    width: '96px',
-    borderRadius: '10px',
-    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)'
-  }
+    '& .MuiPaper-root': {
+        width: '96px',
+        borderRadius: '10px',
+        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)'
+    }
 });
 
 const options = [
-  'View',
-  'Move',
-  'Remove'
+    'View',
+    'Move',
+    'Remove'
 ];
 
 export default function ItemMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  const handleOptionClick = (option: string) => {
-    switch (option) {
-      case 'View':
-        console.log('View was clicked');
-        break;
-      case 'Move':
-        console.log('Move was clicked');
-        break;
-      case 'Remove':
-        console.log('Remove was clicked');
-        break;
-      default:
-        break;
-    }
-    handleClose();
-  };
+    const handleOptionClick = (option: string) => {
+        switch (option) {
+            case 'View':
+                console.log('View was clicked');
+                break;
+            case 'Move':
+                console.log('Move was clicked');
+                break;
+            case 'Remove':
+                console.log('Remove was clicked');
+                break;
+            default:
+                break;
+        }
+        handleClose();
+    };
 
-  return (
-    <div>
-      <IconButton
-        aria-label="more"
-        id="long-button"
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <MoreVertIcon />
-      </IconButton>
-      <StyledMenu
-        id="long-menu"
-        MenuListProps={{
-          'aria-labelledby': 'long-button',
-        }}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'center',
-          horizontal: 'right'
-        }}
-        open={open}
-        onClose={handleClose}
-      >
-        {options.map((option) => (
-          <StyledMenuItem 
-          key={option} onClick={() => handleOptionClick(option)}>
-            {option}
-          </StyledMenuItem>
-        ))}
-      </StyledMenu>
-    </div>
-  );
+    return (
+        <div>
+            <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? 'long-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <StyledMenu
+                id="long-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'right'
+                }}
+                open={open}
+                onClose={handleClose}
+            >
+                {options.map((option) => (
+                    <StyledMenuItem
+                        key={option} onClick={() => handleOptionClick(option)}>
+                        {option}
+                    </StyledMenuItem>
+                ))}
+            </StyledMenu>
+        </div>
+    );
 }
