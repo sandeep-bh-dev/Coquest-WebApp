e67734f218115d6bd71d913062a14956d4f080e2
Ali Amin <aliamin745@gmail.com>
2023-02-19 12:19:19 -0500

    Refactored CreateProgram to fit new changes.

--- frontend/src/pages/Programs/CreateProgram/CreateProgram.tsx
+++ frontend/src/pages/Programs/CreateProgram/CreateProgram.tsx
@@ -1,11 +1,57 @@
 import React, { useState, useEffect } from "react";
-import ProgramCreateProgress from "../ProgramComponents/ProgramCreateProgress";
 import { Button } from "@mui/material";
 import { styled } from "@mui/system";
 import { Outlet } from "react-router-dom";
 import { useLocation, Link } from "react-router-dom";
+import ProgressStepper from "../ProgramComponents/ProgressStepper";
 
-export const CreateProgram = () => {
+const Container = styled("div")({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	flexDirection: "column",
+});
+
+const NavigateButtons = styled("div")({
+	width: "100%",
+	display: "flex",
+	justifyContent: "end",
+	marginTop: 28,
+	marginRight: 73,
+	"& button:hover": {
+		backgroundColor: "#c7c5c5",
+	},
+});
+
+const ProgressBarContainer = styled("div")({
+	marginTop: 40,
+	minWidth: 300,
+	width: "60vw",
+	maxWidth: 700,
+});
+
+const BackButton = styled(Button)({
+	color: "black",
+	fontWeight: 700,
+	textTransform: "none",
+	fontSize: 16,
+	borderRadius: 30,
+});
+
+const NextButton = styled(Button)({
+	marginLeft: 40,
+	backgroundColor: "rgb(217, 217, 217)",
+	color: "black",
+	fontWeight: 700,
+	textTransform: "none",
+	fontSize: 16,
+	borderRadius: 30,
+});
+const getCurrPath = (fullpath: string) => {
+	return fullpath.substring(17, fullpath.length);
+};
+
+const CreateProgram = () => {
 	const location = useLocation();
 	const [pageIndex, setPageIndex] = useState(0);
 	const [currentPath, setCurrentPath] = useState(
@@ -21,7 +67,7 @@ export const CreateProgram = () => {
 	return (
 		<Container>
 			<ProgressBarContainer>
-				<ProgramCreateProgress pgnum={pageIndex} />
+				<ProgressStepper pgnum={pageIndex} />
 			</ProgressBarContainer>
 			<NavigateButtons>
 				{pageIndex > 0 && (
@@ -33,17 +79,7 @@ export const CreateProgram = () => {
 							setPageIndex(pageIndex - 1);
 						}}
 					>
-						<Button
-							style={{
-								color: "black",
-								fontWeight: 700,
-								textTransform: "none",
-								fontSize: 16,
-								borderRadius: 30,
-							}}
-						>
-							{"<"} Back
-						</Button>
+						<BackButton>{"<"} Back</BackButton>
 					</Link>
 				)}
 				{pageIndex < paths.length - 1 && (
@@ -56,21 +92,9 @@ export const CreateProgram = () => {
 							setPageIndex(pageIndex + 1);
 						}}
 					>
-						<Button
-							variant="contained"
-							disableElevation
-							style={{
-								marginLeft: 40,
-								backgroundColor: "rgb(217, 217, 217)",
-								color: "black",
-								fontWeight: 700,
-								textTransform: "none",
-								fontSize: 16,
-								borderRadius: 30,
-							}}
-						>
+						<NextButton variant="contained" disableElevation>
 							Next
-						</Button>
+						</NextButton>
 					</Link>
 				)}
 			</NavigateButtons>
@@ -79,27 +103,4 @@ export const CreateProgram = () => {
 	);
 };
 
-const Container = styled("div")({
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	flexDirection: "column",
-});
-
-const NavigateButtons = styled("div")({
-	width: "100%",
-	display: "flex",
-	justifyContent: "end",
-	marginTop: 28,
-	marginRight: 73,
-});
-
-const ProgressBarContainer = styled("div")({
-	minWidth: 300,
-	width: "60vw",
-	maxWidth: 700,
-});
-
-const getCurrPath = (fullpath: string) => {
-	return fullpath.substring(17, fullpath.length);
-};
+export default CreateProgram;
