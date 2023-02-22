cc9b345567bc9bb3301cdd5f828f2a00af4b9e86
geraldjet <geraldpromchuakaew@gmail.com>
2023-02-22 11:39:23 -0800

    started scheduling page

---
+++ frontend/src/pages/Coop/Scheduling.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Scheduling() {
+    return <div></div>;
+}
+
+export default Scheduling;
--- frontend/src/pages/Coop/index.ts
+++ frontend/src/pages/Coop/index.ts
@@ -1,1 +1,2 @@
 export {default as CreateCoop} from "./CreateCoop"
+export {default as Scheduling} from "./CreateCoop"
