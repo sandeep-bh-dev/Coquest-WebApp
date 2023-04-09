19bba2ffcb1dab5d848700c0c747e9ee0ae58244
Ali Amin <aliamin745@gmail.com>
2023-04-09 00:14:44 -0400

    Fixed imports

--- frontend/src/pages/Coop/CreateCoop/CreateCoop.tsx
+++ frontend/src/pages/Coop/CreateCoop/CreateCoop.tsx
@@ -7,101 +7,101 @@ import { useLocation, Link } from "react-router-dom";
 import ProgressStepper from "../CoopComponents/ProgressStepper";
 
 const Container = styled("div")({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	flexDirection: "column",
 });
 
 const NavigateButtons = styled("div")({
-    width: "100%",
-    display: "flex",
-    justifyContent: "end",
-    marginTop: 28,
-    marginRight: 73,
-    "& button:hover": {
-        backgroundColor: "#c7c5c5",
-    },
+	width: "100%",
+	display: "flex",
+	justifyContent: "end",
+	marginTop: 28,
+	marginRight: 73,
+	"& button:hover": {
+		backgroundColor: "#c7c5c5",
+	},
 });
 
 const ProgressBarContainer = styled("div")({
-    marginTop: 40,
-    minWidth: 300,
-    width: "60vw",
-    maxWidth: 700,
+	marginTop: 40,
+	minWidth: 300,
+	width: "60vw",
+	maxWidth: 700,
 });
 
 const BackButton = styled(Button)({
-    color: "black",
-    fontWeight: 700,
-    textTransform: "none",
-    fontSize: 16,
-    borderRadius: 30,
+	color: "black",
+	fontWeight: 700,
+	textTransform: "none",
+	fontSize: 16,
+	borderRadius: 30,
 });
 
 const NextButton = styled(Button)({
-    marginLeft: 40,
-    backgroundColor: "rgb(217, 217, 217)",
-    color: "black",
-    fontWeight: 700,
-    textTransform: "none",
-    fontSize: 16,
-    borderRadius: 30,
+	marginLeft: 40,
+	backgroundColor: "rgb(217, 217, 217)",
+	color: "black",
+	fontWeight: 700,
+	textTransform: "none",
+	fontSize: 16,
+	borderRadius: 30,
 });
 const getCurrPath = (fullpath: string) => {
-    return fullpath.substring(17, fullpath.length);
+	return fullpath.substring(17, fullpath.length);
 };
 
 const CreateCoop = () => {
-    const location = useLocation();
-    const [pageIndex, setPageIndex] = useState(0);
-    const [currentPath, setCurrentPath] = useState(
-        getCurrPath(location.pathname)
-    );
-    const paths = ["basic-information", "operations", "budgeting", "promotion"];
+	const location = useLocation();
+	const [pageIndex, setPageIndex] = useState(0);
+	const [currentPath, setCurrentPath] = useState(
+		getCurrPath(location.pathname)
+	);
+	const paths = ["basic-information", "operations", "budgeting", "promotion"];
 
-    useEffect(() => {
-        setPageIndex(paths.indexOf(currentPath));
-    }, []);
+	useEffect(() => {
+		setPageIndex(paths.indexOf(currentPath));
+	}, []);
 
-    console.log(currentPath);
-    return (
-        <Container>
-            <ProgressBarContainer>
-                <ProgressStepper pgnum={pageIndex} />
-            </ProgressBarContainer>
-            <NavigateButtons>
-                {pageIndex > 0 && (
-                    <Link
-                        style={{ textDecoration: "none" }}
-                        to={`${paths[pageIndex - 1]}`}
-                        onClick={() => {
-                            setCurrentPath(getCurrPath(location.pathname));
-                            setPageIndex(pageIndex - 1);
-                        }}
-                    >
-                        <BackButton>{"<"} Back</BackButton>
-                    </Link>
-                )}
-                {pageIndex < paths.length - 1 && (
-                    <Link
-                        style={{ textDecoration: "none" }}
-                        to={`${paths[pageIndex + 1]}`}
-                        onClick={() => {
-                            setCurrentPath(getCurrPath(location.pathname));
-                            console.log(pageIndex);
-                            setPageIndex(pageIndex + 1);
-                        }}
-                    >
-                        <NextButton variant="contained" disableElevation>
-                            Next
-                        </NextButton>
-                    </Link>
-                )}
-            </NavigateButtons>
-            <Outlet />
-        </Container>
-    );
+	console.log(currentPath);
+	return (
+		<Container>
+			<ProgressBarContainer>
+				<ProgressStepper pgnum={pageIndex} />
+			</ProgressBarContainer>
+			<NavigateButtons>
+				{pageIndex > 0 && (
+					<Link
+						style={{ textDecoration: "none" }}
+						to={`${paths[pageIndex - 1]}`}
+						onClick={() => {
+							setCurrentPath(getCurrPath(location.pathname));
+							setPageIndex(pageIndex - 1);
+						}}
+					>
+						<BackButton>{"<"} Back</BackButton>
+					</Link>
+				)}
+				{pageIndex < paths.length - 1 && (
+					<Link
+						style={{ textDecoration: "none" }}
+						to={`${paths[pageIndex + 1]}`}
+						onClick={() => {
+							setCurrentPath(getCurrPath(location.pathname));
+							console.log(pageIndex);
+							setPageIndex(pageIndex + 1);
+						}}
+					>
+						<NextButton variant="contained" disableElevation>
+							Next
+						</NextButton>
+					</Link>
+				)}
+			</NavigateButtons>
+			<Outlet />
+		</Container>
+	);
 };
 
 export default CreateCoop;
