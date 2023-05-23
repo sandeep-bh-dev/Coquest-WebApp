36852d872ef3d51631feae88e19479aab97dea3a
Ali Amin <aliamin745@gmail.com>
2023-05-23 18:54:19 +0300

    Program list fixed

--- frontend/src/pages/Programs/ProgramView/ProgramViewComponents/ProgramListDisplay.tsx
+++ frontend/src/pages/Programs/ProgramView/ProgramViewComponents/ProgramListDisplay.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { Button, Typography, TextField, Grid, Card } from "@mui/material";
+import React from "react";
 import { styled } from "@mui/system";
 import ProgramPane from "./ProgramPane";
 
@@ -7,31 +6,71 @@ const Container = styled("div")({
 	display: "flex",
 	margin: "auto",
 	marginTop: 40,
-	width: 835,
+	width: "100%",
 	alignItems: "center",
-	justifyContent: "center",
 	flexDirection: "column",
+	overflowY: "scroll",
+	height: 600,
+	"&::-webkit-scrollbar": {
+		width: "0.5em",
+	},
+	"&::-webkit-scrollbar-thumb": {
+		backgroundColor: "rgba(0,0,0,.5)",
+		borderRadius: "10px",
+	},
+	"&::-webkit-scrollbar-track": {
+		backgroundColor: "rgba(0,0,0,.2)",
+		borderRadius: "10px",
+	},
 });
 interface Program {
 	name: string;
 	location: string;
 	description: string;
-	progress: number;
-	time: string;
-	date: string;
-	openSpots: number;
+	progress: number | null;
+	time: string | null;
+	date: string | null;
+	openSpots: number | null;
 }
 interface ProgramProps {
 	programList: Program[];
 }
+const Breaker = styled("hr")({
+	height: 2,
+	width: "90%",
+});
+
 const ProgramListDisplay = (props: ProgramProps) => {
 	return (
 		<Container>
 			{props.programList.map((program, index) => {
-				return <ProgramPane program={program} key={index} />;
+				return (
+					<>
+						{validateProgram(program) && (
+							<>
+								<ProgramPane program={program} key={index} />
+
+								{index + 1 <= props.programList.length && (
+									<Breaker></Breaker>
+								)}
+							</>
+						)}
+					</>
+				);
 			})}
 		</Container>
 	);
 };
-
+const validateProgram = (program: Program) => {
+	if (
+		program === null ||
+		program.name === null ||
+		program.name === "" ||
+		program.description === null ||
+		program.description === ""
+	) {
+		return false;
+	}
+	return true;
+};
 export default ProgramListDisplay;
