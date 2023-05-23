66d1a7ea28505d6894ffc2d382de578df4940177
Ali Amin <aliamin745@gmail.com>
2023-05-23 18:54:51 +0300

    Add test data.

--- frontend/src/testing/TestProgramsData.tsx
+++ frontend/src/testing/TestProgramsData.tsx
@@ -1,4 +1,4 @@
-export const programs = [
+export const templatePrograms = [
 	{
 		name: "Program name",
 		location: "Location",
@@ -28,5 +28,235 @@ export const programs = [
 		time: "Mon and Wed, 6:00 - 6:30 pm",
 		date: "Jan 1 - 31, 2023",
 		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+	{
+		name: "Program name",
+		location: "Location",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni at odit ea maxime vel, vero, cum veritatis commodi ex dolorem similique. Similique quae blanditiis molestias eos est nulla explicabo iusto?",
+		progress: 15,
+		time: "Mon and Wed, 6:00 - 6:30 pm",
+		date: "Jan 1 - 31, 2023",
+		openSpots: 4,
+	},
+];
+
+export const populatedPrograms = [
+	{
+		name: "Yoga in the Park",
+		location: "Central Park",
+		description:
+			"Join us for a relaxing yoga session in the beautiful Central Park. All levels welcome.",
+		progress: 50,
+		time: "Saturdays, 9:00 - 10:00 am",
+		date: "June 1 - August 31, 2023",
+		openSpots: 10,
+	},
+	{
+		name: "Summer Concert Series",
+		location: "Town Square",
+		description:
+			"Enjoy live music from local bands every Friday night this summer in Town Square.",
+		progress: 25,
+		time: "Fridays, 7:00 - 9:00 pm",
+		date: "June 7 - August 30, 2023",
+		openSpots: 50,
+	},
+	{
+		name: "Farmers Market",
+		location: "City Hall",
+		description:
+			"Support local farmers and vendors at our weekly farmers market. Fresh produce, baked goods, and more!",
+		progress: 75,
+		time: "Sundays, 8:00 am - 12:00 pm",
+		date: "June 2 - September 29, 2023",
+		openSpots: 0,
+	},
+	{
+		name: "Art Walk",
+		location: "Downtown",
+		description:
+			"Explore the vibrant art scene in our city with a guided art walk through downtown galleries and studios.",
+		progress: 40,
+		time: "First Friday of the month, 6:00 - 9:00 pm",
+		date: "June - December, 2023",
+		openSpots: 15,
+	},
+	{
+		name: "Beach Cleanup",
+		location: "Ocean Beach",
+		description:
+			"Help keep our beaches clean and beautiful by joining our monthly beach cleanup event. Gloves and bags provided.",
+		progress: 60,
+		time: "Last Saturday of the month, 9:00 am -12:00 pm",
+		date: "June - September, 2023",
+		openSpots: 20,
+	},
+	{
+		name: "Outdoor Movie Night",
+		location: "Community Park",
+		description:
+			"Bring a blanket and enjoy a family-friendly movie under the stars at our monthly outdoor movie night.",
+		progress: 80,
+		time: "Third Saturday of the month, starts at dusk",
+		date: "June - August, 2023",
+		openSpots: 0,
+	},
+	{
+		name: "5K Fun Run",
+		location: "",
+		description:
+			"Get active and have fun at our annual 5K fun run. All ages and abilities welcome.",
+		progress: null,
+		time: "August 10, starts at 8 am",
+		date: "",
+		openSpots: 25,
+	},
+	{
+		name: "Food Festival",
+		location: "Main Street",
+		description:
+			"Taste the best of our city's cuisine at the annual food festival. Sample dishes from local restaurants and food trucks.",
+		progress: 90,
+		time: "",
+		date: "",
+		openSpots: null,
+	},
+	{
+		name: "",
+		location: "",
+		description: "",
+		progress: null,
+		time: "",
+		date: "",
+		openSpots: null,
+	},
+	{
+		name: "",
+		location: "",
+		description: "",
+		progress: null,
+		time: "",
+		date: "",
+		openSpots: null,
+	},
+	{
+		name: "",
+		location: "",
+		description: "",
+		progress: null,
+		time: "",
+		date: "",
+		openSpots: null,
 	},
 ];
