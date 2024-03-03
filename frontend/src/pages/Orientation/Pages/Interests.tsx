6ee6783a5eb0ecbcfa2fa9b69ad2a411d2b21a25
Nicholas Defranco <defrann8208@outlook.com>
2024-03-03 17:06:48 -0500

    Add Interest edit page

--- frontend/src/pages/Orientation/Pages/Interests.tsx
+++ frontend/src/pages/Orientation/Pages/Interests.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import onCheck from "./utils";
+import { userModel, userObservable } from "../../../models/userobserver";
+import { Topic } from "../../../models/common";
+import { capitalize } from "./utils";
 
-function Interests() {
+function Interests(props: any) {    
+    const [user, setUser] = useState(userModel);
+    const [topics, setTopics] = useState(new Set<string>(user.topics));
+
+    // Watch changes to shared userModel
+    useEffect(() => {
+        const subscription = userObservable.subscribe(setUser);
+        return () => {
+            // TODO: PUT updated data to the back-end API
+            subscription.unsubscribe();
+        }
+    }, []);
+
     return (
-        <div>Interests</div>
+        <div>
+            <p>What are your interests?</p>
+            
+            {Object.values(Topic).map(
+                    (topic) => (
+                        <div key={topic}>
+                            <input
+                                onChange={(e) => onCheck([setTopics, props.updateData], topics, e)}
+                                type="checkbox"
+                                id={topic.toLowerCase()}
+                                name={topic}
+                                defaultChecked={topics.has(topic)} />
+                            <label htmlFor={topic.toLowerCase()}>{capitalize(topic)}</label>
+                        </div>
+                    )
+                )}
+        </div>
     );
 }
 
