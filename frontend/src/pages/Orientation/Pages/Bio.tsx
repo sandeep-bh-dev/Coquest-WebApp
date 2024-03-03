56f89eafc48a0978eff61cb083a7cc5d6b132bc5
Nicholas Defranco <defrann8208@outlook.com>
2024-03-03 17:05:00 -0500

    Add Bio edit page
    amend

--- frontend/src/pages/Orientation/Pages/Bio.tsx
+++ frontend/src/pages/Orientation/Pages/Bio.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { userModel, userObservable } from "../../../models/userobserver";
 
-function Bio() {
+const MAX_CHAR_COUNT = 1000;
+
+function Bio(props: any) {
+    const [user, setUser] = useState(userModel);
+    const [biography, setBiography] = useState(user.biography);
+
+    // Watch changes to shared userModel
+    useEffect(() => {
+        const subscribtion = userObservable.subscribe(setUser);
+        return () => {
+            // TODO: PUT updated data to the back-end API
+            subscribtion.unsubscribe();
+        }
+    }, []);
+
+    function onEdit(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        const newBio = e.target.value
+        setBiography(newBio);
+        props.updateData(newBio);
+    }
+
     return (
-        <div>Bio</div>
+        <div>
+            <h1>Welcome to Regenquest&#44; {userModel.name}</h1>
+            <textarea
+                value={biography}
+                onChange={onEdit}
+                maxLength={MAX_CHAR_COUNT}>
+            </textarea>
+            <p>{biography.length}&nbsp;/&nbsp;{MAX_CHAR_COUNT}</p>
+        </div>
     );
 }
 
